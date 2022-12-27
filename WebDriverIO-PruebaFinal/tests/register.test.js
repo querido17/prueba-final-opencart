b0f5700df1b990c4458da65386fc4250b3fdd984
carolina17 <carolina_arge@hotmail.com>
2022-12-27 15:31:22 -0300

    Completado CP07
    Se agrega json con datos para terminar CP07

--- WebDriverIO-PruebaFinal/tests/register.test.js
+++ WebDriverIO-PruebaFinal/tests/register.test.js
@@ -3,6 +3,7 @@ import { expect } from "chai";
 import homePage from '../pages/home.page';
 import busquedaPage from '../pages/busqueda.page';
 import registerPage from '../pages/register.page';
+import { DATA } from '../data/registerData.json';
 
 describe('OpenCart Register', () => {
 
@@ -14,7 +15,16 @@ describe('OpenCart Register', () => {
         expect(await registerPage.registerContainer.isDisplayedInViewport(), 'Error: no se ingresó a Register Page').to.be.true;
 
         // Completar los campos
-        
+        await registerPage.completeFirstName(DATA.firstName);
+        await registerPage.completeLastName(DATA.lastName);
+        await registerPage.completeEmail(DATA.email);
+        await registerPage.completeTelephone(DATA.telephone);
+        await registerPage.completePassword(DATA.password);
+        await registerPage.confirmPassword(DATA.password);
+
+        // Aceptar Privacy Policy y confirmar el registro
+        await registerPage.acceptPrivacyPolicy();
+        await registerPage.confirmRegistry();
 
         await browser.pause(5000);
     });
--- WebDriverIO-PruebaFinal/wdio.conf.js
+++ WebDriverIO-PruebaFinal/wdio.conf.js
@@ -218,8 +218,10 @@ exports.config = {
     /**
      * Function to be executed before a test (in Mocha/Jasmine) starts.
      */
-    // beforeTest: function (test, context) {
-    // },
+     beforeTest: function (test, context) {
+        const { addStep } = require('@wdio/allure-reporter').default
+        global.addStep = addStep
+     },
     /**
      * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
      * beforeEach in Mocha)
