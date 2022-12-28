db3245d4dcd82f5ed1809e9daac963d83e24f41c
carolina17 <carolina_arge@hotmail.com>
2022-12-28 11:08:10 -0300

    CP08 y CP09
    Added CP08 and CP09

--- WebDriverIO-PruebaFinal/tests/register.test.js
+++ WebDriverIO-PruebaFinal/tests/register.test.js
@@ -28,4 +28,47 @@ describe('OpenCart Register', () => {
 
         await browser.pause(5000);
     });
+
+    it('[CP08] No debería registrar al usuario al ingresar E-Mail asociado a otro usuario', async () => { 
+        // Ingreso a la pantalla register
+        await homePage.abrir('/');
+        assert.equal(await homePage.getHomeTitle(), 'Your Store', 'Error: no se ingresó a la pantalla de inicio');
+        await homePage.entrarAlRegister();
+        expect(await registerPage.registerContainer.isDisplayedInViewport(), 'Error: no se ingresó a Register Page').to.be.true;
+
+        // Completar los campos utilizando E-Mail asociado a otro usuario
+        await registerPage.completeFirstName(DATA.firstName);
+        await registerPage.completeLastName(DATA.lastName);
+        await registerPage.completeEmail(DATA.email);
+        await registerPage.completeTelephone(DATA.telephone);
+        await registerPage.completePassword(DATA.password);
+        await registerPage.confirmPassword(DATA.password);
+
+        // Aceptar Privacy Policy y confirmar el registro
+        await registerPage.acceptPrivacyPolicy();
+        await registerPage.confirmRegistry();
+
+        await browser.pause(5000);
+    });
+
+    it('[CP09] No debería registrar al usuario al ingresar datos válidos pero no aceptar la política de privacidad', async () => { 
+        // Ingreso a la pantalla register
+        await homePage.abrir('/');
+        assert.equal(await homePage.getHomeTitle(), 'Your Store', 'Error: no se ingresó a la pantalla de inicio');
+        await homePage.entrarAlRegister();
+        expect(await registerPage.registerContainer.isDisplayedInViewport(), 'Error: no se ingresó a Register Page').to.be.true;
+
+        // Completar los campos
+        await registerPage.completeFirstName(DATA.firstName);
+        await registerPage.completeLastName(DATA.lastName);
+        await registerPage.completeEmail(DATA.email);
+        await registerPage.completeTelephone(DATA.telephone);
+        await registerPage.completePassword(DATA.password);
+        await registerPage.confirmPassword(DATA.password);
+
+        // No aceptar Privacy Policy y confirmar el registro
+        await registerPage.confirmRegistry();
+
+        await browser.pause(5000);
+    });
   });
