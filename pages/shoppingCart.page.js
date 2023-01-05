f23a0d2cb2cd9efc36bf0da4c5def1a1999af59c
carolina17 <carolina_arge@hotmail.com>
2023-01-05 14:47:49 -0300

    cp05
    cp05

--- pages/shoppingCart.page.js
+++ pages/shoppingCart.page.js
@@ -4,7 +4,8 @@ import BasePage from '../pages/base.page';
 
 class ShoppingCartPage extends BasePage {
     //WebElements
-    get addToCartBtn() {return $('#button-cart') }
+    get addToCartBtn() {return $('#button-cart') };
+    get successMsg() {return $('.alert-success')};
 
     //-------------------------------------------------------------------------------------------------------//
 
@@ -12,6 +13,10 @@ class ShoppingCartPage extends BasePage {
         addStep('Add to cart');
         await super.clickearElemento(this.addToCartBtn);
     }
+
+    /* async getSuccessMsg() {
+        return await (await this.successMsg).getText();
+    } */
  }
  export default new ShoppingCartPage();
  
--- tests/shoppingCart.test.js
+++ tests/shoppingCart.test.js
@@ -14,9 +14,14 @@ describe('OpenCart Shopping Cart', () => {
             await homePage.abrir('/');
             expect(await homePage.carrusel.isDisplayedInViewport(), 'Error: no se ingresó a la pantalla de inicio').to.be.true;
         });
-            it(`[CP05] Debería agregar ${item} al carrito de compras`, async ()=> {
+           it(`[CP05] Debería agregar ${item} al carrito de compras`, async ()=> {
                 await homePage.clickearElemento(await homePage.getProduct(item));
+                expect(await (homePage.getProduct(item)).isDisplayedInViewport(), `Error: no se ingresó a ${item}`).to.be.true;
+                await shoppingCartPage.addToCart();
+                expect(await shoppingCartPage.successMsg.isDisplayedInViewport(), `Error: No se agregó ${item} al carrito de compras`).to.be.true;
+                // assert.equal(await shoppingCartPage.getSuccessMsg(), `Success: You have added ${item} to your `, 'Error: debería mostrarse mensaje de éxito');
                 await homePage.returnHome();
+                expect(await homePage.carrusel.isDisplayedInViewport(), 'Error: no se ingresó a la pantalla de inicio').to.be.true;
             });
     });
  });
